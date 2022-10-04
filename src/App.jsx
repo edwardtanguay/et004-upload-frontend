4430d323c94d77c5e1277d543660596df6f7a96c
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-04 23:33:58 +0200

    lowdb in and showing file items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
+import axios from 'axios';
 
 const backendUrl = 'http://localhost:5889';
+const _initialFormFields = {
+	title: '',
+	description: '',
+	notes: ''
+};
+const _initialUploadFile = {
+	preview: '',
+	data: '',
+	name: ''
+};
 
 function App() {
-	const [uploadFile, setUploadFile] = useState({ preview: '', data: '' });
+	const [uploadFile, setUploadFile] = useState({ ..._initialUploadFile });
+	const [formFields, setFormFields] = useState({ ..._initialFormFields });
 	const [status, setStatus] = useState('');
+	const [fileItems, setFileItems] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setFileItems((await axios.get(`${backendUrl}/fileitems`)).data);
+		})();
+	}, []);
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		let formData = new FormData();
@@ -15,11 +35,14 @@ function App() {
 			body: formData
 		});
 		if (response) setStatus(response.statusText);
+		document.getElementById('mainForm').reset();
+		setFormFields({ ..._initialFormFields });
+		setUploadFile({ ..._initialUploadFile });
 	};
 
 	const handleFileChange = (e) => {
 		const file = e.target.files[0];
-    setStatus('');
+		setStatus('');
 		const _uploadFile = {
 			name: file.name,
 			preview: URL.createObjectURL(file),
@@ -28,34 +51,83 @@ function App() {
 		setUploadFile(_uploadFile);
 	};
 
+	const handleFormFieldChange = (e, fieldName) => {
+		const value = e.target.value;
+		formFields[fieldName] = value;
+		setFormFields({ ...formFields });
+	};
+
 	return (
 		<div className="App">
 			<h1>File Uploader</h1>
-			<hr />
-			<form onSubmit={handleSubmit}>
+			<main>
+				<section>
+					<form id="mainForm" onSubmit={handleSubmit}>
+						<fieldset>
+							<legend>Enter file info and choose file:</legend>
 
-				<input type="file" onChange={handleFileChange}></input>
-				<button type="submit">Upload Now</button>
-			</form>
-			<hr />
-			{status !== 'OK' && uploadFile.preview && (
-				<>
-					<div className="uploadMessage">
-						FILE TO UPLOAD:{' '}
-						<span className="uploadFileName">
-							{uploadFile.name}
-						</span>
-					</div>
-					{uploadFile.name.endsWith('.jpg') && (
-						<img
-							src={uploadFile.preview}
-							width="100"
-							height="100"
-						/>
-					)}
-				</>
-			)}
-			{status === 'OK' && <h4>File was uploaded.</h4>}
+							<label htmlFor="title">Title</label>
+							<input
+								type="text"
+								id="title"
+								autoFocus
+								value={formFields.title}
+								onChange={(e) =>
+									handleFormFieldChange(e, 'title')
+								}
+							/>
+
+							<label htmlFor="description">Description</label>
+							<input
+								type="text"
+								id="description"
+								value={formFields.description}
+								onChange={(e) =>
+									handleFormFieldChange(e, 'description')
+								}
+							/>
+
+							<label htmlFor="notes">Notes</label>
+							<input
+								type="text"
+								value={formFields.notes}
+								onChange={(e) =>
+									handleFormFieldChange(e, 'notes')
+								}
+							/>
+
+							<label>File to upload</label>
+							<input
+								type="file"
+								onChange={handleFileChange}
+							></input>
+							<div className="buttonArea">
+								<div className="preview">
+									{!uploadFile.name.endsWith('.jpg') && (
+										<div className="previewFileName">
+											{uploadFile.name}
+										</div>
+									)}
+									{uploadFile.name.endsWith('.jpg') && (
+										<img
+											src={uploadFile.preview}
+											width="100"
+											height="100"
+										/>
+									)}
+								</div>
+								<div className="buttonWrapper">
+									<button type="submit">Submit</button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</section>
+				<section className="fileItemsArea">
+					<h2>File Items</h2>
+					<p>There are {fileItems.length} file items</p>
+				</section>
+			</main>
 		</div>
 	);
 }
